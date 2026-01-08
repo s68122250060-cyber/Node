ce4ba7e294b962112a58d0d16f3e2785696ee781
s68122250060-cyber <s68122250060@ssru.ac.th>
2026-01-08 14:59:41 +0700

    first commit

---
+++ src/TestRandom.java
@@ -0,0 +1,28 @@
+
+import java.util.Random;
+
+public class TestRandom {
+    public static void main(String[] args) {
+        int[] T = new int[10]; // สร้างอาเรย์ขนาด 10
+        Random rand = new Random();
+        int sum = 0;
+
+        // สุ่มตัวเลขระหว่าง 50 ถึง 59
+        for (int i = 0; i < T.length; i++) {
+            T[i] = rand.nextInt(10) + 50; // สุ่ม 0-9 แล้วบวก 50
+        }
+
+        // แสดงค่าและหาผลรวม
+        System.out.print("Numbers: ");
+        for (int i : T) {
+            System.out.print(i + " ");
+            sum += i;
+        }
+
+        // คำนวณค่าเฉลี่ย
+        double avg = (double) sum / T.length;
+
+        System.out.println("\nSum = " + sum);
+        System.out.println("Average = " + avg);
+    }
+}
